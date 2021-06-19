0b1a435cdf9135f82d7a1e6bc31dfe15672b8e93
Whispers12 <frolovandrey752@gmail.com>
2021-06-19 18:34:36 +0400

    feat(validation): add some ideas for rules

---
+++ src/validation/rules/allowOnlyShorthandProperties.rule.ts
@@ -0,0 +1,21 @@
+/**
+ * 
+ * 
+ * good ✅
+ * 
+ * AcDocument {
+ *    field: d,
+ *    field2: s,
+ * }
+ * 
+ * bad ❌
+ * 
+ * Document {
+ *   field: string,
+ *  field2: datetime
+ * }
+ * 
+ * 
+ */
+
+ class AllowOnlyShorthandProperties {}
