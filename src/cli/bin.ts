44cfea122069f2fa317a0db71ad6843c356c6b0c
Whispers12 <frolovandrey752@gmail.com>
2021-07-12 20:24:32 +0400

    update cli

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import path from 'path';
+import { default as getPath } from 'path';
 import { readFile } from 'fs/promises';
 import { parse } from '../parser';
 import { AnySpecSchema } from '../runtypes';
 import { validate, baseRules } from '../validation';
-import { printError } from '../error';
+import { AnySpecError, printError } from '../error';
+import { sync as glob } from 'globby';
+import { Source } from '../source';
+import ora from 'ora';
+import { ASTNodeKind, TypeDefinitionNode } from '../language';
+
+function isEndpoint(val: string) {
+  return val.includes('.endpoints.');
+}
+
+function isModel(val: string) {
+  return val.includes('.models.');
+}
 
 async function main() {
   const program = new Command();
@@ -18,16 +30,51 @@ async function main() {
 
   const { args } = program;
 
-  const specFiles = args.map((arg) => path.resolve(process.cwd(), arg));
+  const argPaths = args.map((arg) => getPath.resolve(process.cwd(), arg));
 
-  const content = await readFile(specFiles[0], { encoding: 'utf-8' });
+  const argumentPath = argPaths[0];
+  const processingSpinner = ora(`Processing spec: ${argumentPath}`).start();
 
-  const doc = parse(content);
+  const specFilePaths = glob(`${argumentPath}/**/*.tinyspec`);
 
-  const schema = new AnySpecSchema({ ast: doc });
-  const errors = validate(schema, doc, baseRules);
+  const specBodyFiles = await Promise.all(
+    specFilePaths.map((file) => readFile(file, { encoding: 'utf-8' })),
+  );
 
-  errors.forEach((e) => console.log(printError(e)));
+  const sources = specFilePaths.map((path, i) => {
+    if (isEndpoint(path)) {
+      return new Source({ body: specBodyFiles[i], sourceType: 'endpoints' });
+    }
+
+    if (isModel(path)) {
+      return new Source({ body: specBodyFiles[i], sourceType: 'models' });
+    }
+
+    throw new Error(`File doesn't contain .endpoints.tinyspec or .models.tinyspec extension`);
+  });
+
+  try {
+    const resultDefinitions: TypeDefinitionNode[] = [];
+    for (const source of sources) {
+      const doc = parse(source);
+
+      resultDefinitions.push(...doc.definitions);
+    }
+
+    const doc = { kind: ASTNodeKind.DOCUMENT, definitions: resultDefinitions };
+    const schema = new AnySpecSchema({ ast: doc });
+
+    const errors = validate(schema, doc, baseRules);
+    errors.forEach((e) => console.log(printError(e)));
+
+    processingSpinner.succeed();
+  } catch (error) {
+    if (error instanceof AnySpecError) {
+      console.log(printError(error));
+    }
+
+    processingSpinner.fail(`Failed to process document`);
+  }
 }
 
 main();
