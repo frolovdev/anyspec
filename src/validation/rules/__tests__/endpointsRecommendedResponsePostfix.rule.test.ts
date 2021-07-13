baa08b88d1ded04909aa223aeadd9c183e94892d
Andrey <aimm.legate@gmail.com>
2021-07-13 23:25:38 +0300

    Response rule

---
+++ src/validation/rules/__tests__/endpointsRecommendedResponsePostfix.rule.test.ts
@@ -0,0 +1,37 @@
+import { toJSONDeep } from '../../../utils';
+import { EndpointsRecommendedQueryPostfix } from '../recommended/endpointsRecommendedResponsePostfix.rule';
+import { expectValidationErrors } from './fixtures';
+
+function getErrors(queryStr: string) {
+  return expectValidationErrors(EndpointsRecommendedQueryPostfix, queryStr, 'endpoints');
+}
+
+function expectValid(queryStr: string) {
+  const errors = getErrors(queryStr);
+
+  expect(errors).toEqual([]);
+}
+
+describe(__filename, () => {
+  it('should be valid', () => {
+    expectValid(`
+GET /endpoint
+  => ResponseModelResponse
+`);
+  });
+
+  it('should be invalid', () => {
+    const errors = getErrors(
+      `
+GET /endpoint
+  => ResponseModel
+`,
+    );
+
+    expect(toJSONDeep(errors)).toMatchObject([
+      {
+        message: 'Request model should ends with Response postfix',
+      },
+    ]);
+  });
+});
--- src/validation/rules/recommended/endpointsRecommendedResponsePostfix.rule.ts
+++ src/validation/rules/recommended/endpointsRecommendedResponsePostfix.rule.ts
@@ -1,16 +1,19 @@
 import { AnySpecError } from '../../../error';
+import { ASTNodeKind } from '../../../language';
 import { ASTVisitor } from '../../../visitor';
 import { ValidationContext } from '../../validationContext';
 
-const POSTFIX = 'RequestQuery';
+const POSTFIX = 'Response';
 
 export function EndpointsRecommendedQueryPostfix(context: ValidationContext): ASTVisitor {
   return {
-    EndpointParameterQuery(node) {
-      if (!node.type.name.value?.endsWith(POSTFIX)) {
-        context.reportError(
-          new AnySpecError(`Query model should ends with RequestQuery postfix`, node),
-        );
+    EndpointResponse(node) {
+      if (node.type.kind === ASTNodeKind.NAMED_TYPE) {
+        if (!node.type.name.value?.endsWith(POSTFIX)) {
+          context.reportError(
+            new AnySpecError(`Request model should ends with Response postfix`, node),
+          );
+        }
       }
     },
   };
