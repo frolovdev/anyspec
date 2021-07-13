b5bf8e3b6278a428af9ca815cfd263c8f591b7c8
Andrey <aimm.legate@gmail.com>
2021-07-13 22:52:17 +0300

    rename

--- src/validation/rules/recommended/endpointsRecommendedQueryPostfix.rule.ts
+++ src/validation/rules/recommended/endpointsRecommendedQueryPostfix.rule.ts
@@ -2,12 +2,12 @@ import { AnySpecError } from '../../../error';
 import { ASTVisitor } from '../../../visitor';
 import { ValidationContext } from '../../validationContext';
 
-const PREFIX = 'RequestQuery';
+const POSTFIX = 'RequestQuery';
 
 export function EndpointsRecommendedQueryPostfix(context: ValidationContext): ASTVisitor {
   return {
     EndpointParameterQuery(node) {
-      if (!node.type.name.value?.endsWith(PREFIX)) {
+      if (!node.type.name.value?.endsWith(POSTFIX)) {
         context.reportError(
           new AnySpecError(`Query model should ends with RequestQuery postfix`, node),
         );
