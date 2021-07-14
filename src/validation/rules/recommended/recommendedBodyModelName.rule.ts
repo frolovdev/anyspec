dfae88f871e413b88eccd0d17035ebcc97250c62
Andrey <aimm.legate@gmail.com>
2021-07-14 15:20:54 +0300

    rule

---
+++ src/validation/rules/recommended/recommendedBodyModelName.rule.ts
@@ -0,0 +1,51 @@
+import { AnySpecError } from '../../../error';
+import { ASTNodeKind } from '../../../language';
+import { ASTVisitor } from '../../../visitor';
+import { ValidationContext } from '../../validationContext';
+
+/**
+ *
+ * if model contains `body` field ensure that model name is substring of body parameter type
+ *
+ * good ✅
+ *
+ * Model {
+ *   body: ModelRequestBody,
+ * }
+ *
+ * bad ❌
+ *
+ * Other {
+ *   body: ModelRequestBody,
+ * }
+ *
+ */
+export function RecommendedBodyModelName(context: ValidationContext): ASTVisitor {
+  return {
+    ModelTypeDefinition(node) {
+      if (!node.fields.some((fieldDefinition) => fieldDefinition.name.value === 'body')) {
+        return;
+      }
+
+      const modelName = node.name.value;
+      const fieldNode = node.fields.find(
+        (fieldDefinition) => fieldDefinition.name.value === 'body',
+      );
+      const fieldTypeName =
+        fieldNode?.type.kind === ASTNodeKind.NAMED_TYPE ? fieldNode.type.name.value : undefined;
+
+      if (!fieldNode) {
+        return;
+      }
+
+      if (!fieldTypeName?.includes(modelName)) {
+        context.reportError(
+          new AnySpecError(
+            `Name of model "${modelName}" should be substring of "body" parameter type "${fieldTypeName}"`,
+            node,
+          ),
+        );
+      }
+    },
+  };
+}
