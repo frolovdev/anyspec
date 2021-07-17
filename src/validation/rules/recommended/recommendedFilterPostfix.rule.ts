fcc9e9b7432f4d790a6d91d69f39a02e56684d24
Andrey <aimm.legate@gmail.com>
2021-07-17 17:39:49 +0300

    fix

--- src/validation/rules/recommended/recommendedFilterPostfix.rule.ts
+++ src/validation/rules/recommended/recommendedFilterPostfix.rule.ts
@@ -1,5 +1,6 @@
 import { AnySpecError } from '../../../error';
 import { ASTNodeKind } from '../../../language';
+import { specifiedScalarTypes } from '../../../runtypes';
 import { ASTVisitor } from '../../../visitor';
 import { ValidationContext } from '../../validationContext';
 
@@ -26,6 +27,12 @@ export function RecommendedFilterPostfix(context: ValidationContext): ASTVisitor
     FieldDefinition(node) {
       if (node.name.value === 'filter') {
         if (node.type.kind === ASTNodeKind.NAMED_TYPE) {
+          if (!node.type.name.value) {
+            return;
+          }
+          if (specifiedScalarTypes.includes(node.type.name.value)) {
+            return;
+          }
           if (!node.type.name.value?.endsWith(POSTFIX)) {
             context.reportError(
               new AnySpecError(
