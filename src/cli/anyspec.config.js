39841adbe06f16ae9ad1c5f5699b61f307814ea6
Whispers12 <frolovandrey752@gmail.com>
2021-07-24 13:16:31 +0400

    update cli

---
+++ src/cli/anyspec.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rules: {
+    'base/known-type-names': 'error',
+    'base/no-explicit-string-rule': 'off',
+    'base/endpoints-known-http-verbs': 'off',
+    'recommended/endpoints-body-parameter-postfix': 'off',
+    'recommended/endpoints-query-postfix': 'off',
+    'recommended/endpoints-response-postfix': 'off',
+    'recommended/body-model-name': 'off',
+    'recommended/filter-postfix': 'off',
+    'recommended/model-body-field-postfix': 'off',
+    'recommended/postfix-for-create-models': 'off',
+    'recommended/postfix-for-update-models': 'off',
+  },
+};
--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -1,45 +1,124 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { default as getPath } from 'path';
+import { default as nodePath } from 'path';
 import { readFile } from 'fs/promises';
-import { parse, Source, ASTNodeKind, TypeDefinitionNode } from '../language';
+import { parse, Source, DocumentNode } from '../language';
 import { AnySpecSchema } from '../runtypes';
-import { validate, baseRules } from '../validation';
+import { validate, rulesMap } from '../validation';
 import { AnySpecError, printError } from '../error';
 import { sync as glob } from 'globby';
 import ora from 'ora';
-
-function isEndpoint(val: string) {
-  return val.includes('.endpoints.');
-}
-
-function isModel(val: string) {
-  return val.includes('.models.');
-}
+import { concatAST } from '../language/concatAST';
+import { parseConfig, readConfig } from './config';
 
 async function main() {
   const program = new Command();
+
   program
-    .requiredOption('-o, --out-dir <dir>', 'path to a directory for a generated SDK')
-    .arguments('<specFiles...>');
+    .option('-o, --outDir <dir>', 'path to a directory for a generated openapi')
+    .option('-ns, --namespaces [namespaces...]', 'array of existed namespaces')
+    .option(
+      '-cns, --commonNamespace <commonNamespace>',
+      'name of common namespace where shared definitions stored',
+      'common',
+    )
+    .option('-c, --config <path>', 'path to config file')
+    .arguments('<specFiles>');
 
   program.parse();
 
   const { args } = program;
 
-  const argPaths = args.map((arg) => getPath.resolve(process.cwd(), arg));
+  const options = program.opts() as {
+    commonNamespace: string;
+    namespaces?: string[];
+    outDir?: string;
+    config?: string;
+  };
+
+  const { namespaces, outDir, commonNamespace, config: configPath } = options;
+
+  if (!namespaces) {
+    throw new Error('please provide namespaces');
+  }
+
+  const argPaths = args.map((arg) => nodePath.resolve(process.cwd(), arg));
 
   const argumentPath = argPaths[0];
   const processingSpinner = ora(`Processing spec: ${argumentPath}`).start();
 
   const specFilePaths = glob(`${argumentPath}/**/*.tinyspec`);
+  try {
+    const sources = await mapPathsToSources(specFilePaths);
 
+    const { res: config, err: configErr } = readConfig(configPath);
+    if (configErr || !config) {
+      console.error(configErr);
+      processingSpinner.fail();
+      return;
+    }
+
+    const groupedSources = groupSourcesByNamespaces({ sources, commonNamespace, namespaces });
+
+    const { groupedParsedDocuments, parsingErrors } = getGroupedDocuments(
+      groupedSources,
+      (error: Error) => {
+        console.error('Unknown error during parsing', error);
+        processingSpinner.fail();
+        process.exit(1);
+      },
+    );
+
+    if (parsingErrors.length > 0) {
+      for (const e of parsingErrors) {
+        console.error(printCliError(printError(e)));
+      }
+
+      processingSpinner.fail();
+      return;
+    }
+
+    const { enabledRules, invalidRules } = parseConfig(config);
+
+    const enabledRulesFns = enabledRules.map((rule) => rulesMap[rule]);
+    const unitedASTs = groupedParsedDocuments.map((documents) => concatAST(documents));
+
+    const schemas = unitedASTs.map((ast) => new AnySpecSchema({ ast }));
+    const errors = schemas.map((s, index) => validate(s, unitedASTs[index], enabledRulesFns));
+
+    errors.flat().forEach((e) => console.error(printCliError(printError(e))));
+    invalidRules.forEach((e) => console.error(printCliError(`Invalid Rule: ${e}`)));
+
+    processingSpinner.succeed();
+  } catch (e) {
+    console.error(e);
+    processingSpinner.fail();
+  }
+}
+
+main();
+
+// private
+
+function isEndpoint(val: string) {
+  return val.match(/\.endpoints\.tinyspec$/) !== null;
+}
+
+function isModel(val: string) {
+  return val.match(/\.models\.tinyspec$/) !== null;
+}
+
+function printCliError(error: string) {
+  return `${error}\n\n------------------------------------------------------------------------------------------\n`;
+}
+
+async function mapPathsToSources(paths: string[]): Promise<Source[]> {
   const specBodyFiles = await Promise.all(
-    specFilePaths.map((file) => readFile(file, { encoding: 'utf-8' })),
+    paths.map((file) => readFile(file, { encoding: 'utf-8' })),
   );
 
-  const sources = specFilePaths.map((path, i) => {
+  const sources = paths.map((path, i) => {
     if (isEndpoint(path)) {
       return new Source({ body: specBodyFiles[i], sourceType: 'endpoints', name: path });
     }
@@ -51,28 +130,54 @@ async function main() {
     throw new Error(`File doesn't contain .endpoints.tinyspec or .models.tinyspec extension`);
   });
 
-  try {
-    const resultDefinitions: TypeDefinitionNode[] = [];
-    for (const source of sources) {
-      const doc = parse(source);
-
-      resultDefinitions.push(...doc.definitions);
-    }
-
-    const doc = { kind: ASTNodeKind.DOCUMENT, definitions: resultDefinitions };
-    const schema = new AnySpecSchema({ ast: doc });
-
-    const errors = validate(schema, doc, baseRules);
-    errors.forEach((e) => console.log(printError(e)));
-
-    processingSpinner.succeed();
-  } catch (error) {
-    if (error instanceof AnySpecError) {
-      console.log(printError(error));
-    }
-
-    processingSpinner.fail(`Failed to process document`);
-  }
+  return sources;
 }
 
-main();
+function groupSourcesByNamespaces({
+  sources,
+  namespaces,
+  commonNamespace,
+}: {
+  sources: Source[];
+  namespaces: string[];
+  commonNamespace: string;
+}): Source[][] {
+  const commonRegexp = new RegExp(`\.${commonNamespace}\.(models|endpoints)\.tinyspec$`);
+
+  const namespacesRegexps = namespaces.map(
+    (n) => new RegExp(`\.${n}\.(models|endpoints)\.tinyspec$`),
+  );
+  const commonSources = sources.filter((s) => commonRegexp.test(s.name));
+
+  const namespaceSources = namespacesRegexps
+    .map((regexp) => sources.filter((s) => regexp.test(s.name)))
+    .filter((sources) => sources.length > 0);
+
+  return namespaceSources.map((sourceArray) => sourceArray.concat(commonSources));
+}
+
+function getGroupedDocuments(groupedSources: Source[][], errCb: (err: Error) => void) {
+  const groupedParsedDocuments: DocumentNode[][] = [];
+
+  const parsingErrors: AnySpecError[] = [];
+  groupedSources.forEach((gS) => {
+    const parsedDocuments: DocumentNode[] = [];
+
+    gS.forEach((s) => {
+      try {
+        const doc = parse(s);
+        parsedDocuments.push(doc);
+      } catch (error) {
+        if (error instanceof AnySpecError) {
+          parsingErrors.push(error);
+        } else {
+          errCb(error);
+        }
+      }
+    });
+
+    groupedParsedDocuments.push(parsedDocuments);
+  });
+
+  return { groupedParsedDocuments, parsingErrors };
+}
