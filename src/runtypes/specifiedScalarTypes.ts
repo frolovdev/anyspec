c3fabd41b55d2515b453d43728ed1cf7ec1c7c59
Andrey <aimm.legate@gmail.com>
2021-07-24 17:17:43 +0300

    add scalars

--- src/runtypes/specifiedScalarTypes.ts
+++ src/runtypes/specifiedScalarTypes.ts
@@ -1,20 +1,38 @@
 export const stringAliases = new Set(['s', 'string']);
+export const integerAliases = new Set(['i', 'integer']);
+export const booleanAliases = new Set(['b', 'boolean']);
+export const objectAliases = new Set(['o', 'object']);
+export const floatAliases = new Set(['f', 'float']);
+export const dateAliases = new Set(['d', 'date', 'datetime']);
+export const textAliases = new Set(['t', 'text']);
+export const jsonAliases = new Set(['j', 'json']);
+
+export const scalarAliases = [
+  stringAliases,
+  integerAliases,
+  booleanAliases,
+  objectAliases,
+  floatAliases,
+  dateAliases,
+  textAliases,
+  jsonAliases,
+];
 
 export const specifiedScalarTypes = new Set([
-  'i',
-  'integer',
+  ...integerAliases,
   ...stringAliases,
-  'b',
-  'boolean',
-  'o',
-  'object',
-  'f',
-  'float',
-  'date',
-  'd',
-  'datetime',
-  't',
-  'text',
-  'j',
-  'json',
+  ...booleanAliases,
+  ...objectAliases,
+  ...floatAliases,
+  ...dateAliases,
+  ...textAliases,
+  ...jsonAliases,
 ]);
+
+export function getNormalizedScalar(scalar: string): string | undefined {
+  const scalars = Array.from(scalarAliases.find((alias) => alias.has(scalar)) ?? []);
+
+  if (scalars.length !== 0) {
+    return scalars[0];
+  }
+}
